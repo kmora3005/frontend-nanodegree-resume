77bd6810a087bd92a1b58ae733335bad0996e806
Kenneth Mora <kmora3005@gmail.com>
2017-01-10 01:56:54 -0600

    Add my resume without map

--- js/helper.js
+++ js/helper.js
@@ -14,7 +14,6 @@ replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
-
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
@@ -22,10 +21,8 @@ var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
-
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
-
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
@@ -48,8 +45,9 @@ var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschoolURL = '<br><a href="#">%data%</a>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineClasses = '<h3 class="onlineCourse-entry">Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
@@ -87,6 +85,7 @@ function logClicks(x,y) {
 
 $(document).click(function(loc) {
   // your code goes here!
+  console.log('x location: ' + loc.pageX + '; y location: ' + loc.pageY);
 });
 
 
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,186 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+function inName(name){
+	names=name.split(" ");
+	var newName=names[0].charAt(0).toUpperCase() + names[0].slice(1)+" "+names[1].toUpperCase();
+	return newName;
+}
+$("#main").append(internationalizeButton);
+
+var bio={
+	"name":"Kenneth Mora",
+	"role":"Web Developer",
+	"contacts":{
+		"mobile":"8647-0102",
+		"email":"kmora3005@gmail.com",
+		"github":"kmora3005",
+		"location":"Cartago"
+		},
+	"welcomeMessage":"Welcome to my resume",
+	"skills":["C#","XML","Javascript","SQL","HTML","CSS","Python"],
+	"biopic":"images/kenneth.jpg"
+	};
+		
+bio.display = function (){
+	var formattedWelcomeMsg=HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+	var formattedName=HTMLheaderName.replace("%data%",bio.name);
+	var formattedRole=HTMLheaderRole.replace("%data%",bio.role);
+	var formattedEmail=HTMLemail.replace("%data%",bio.contacts.email);
+	var formattedMobile=HTMLmobile.replace("%data%",bio.contacts.mobile);
+	var formattedGithub=HTMLgithub.replace("%data%",bio.contacts.github);
+	var formattedLocation=HTMLlocation.replace("%data%",bio.contacts.location);
+	var formattedPicture=HTMLbioPic.replace("%data%",bio.biopic);
+
+	var idContacts=["#topContacts","#footerContacts"];
+	for (var index=0; index<idContacts.length; index++){
+		$(idContacts[index]).append(formattedEmail);
+		$(idContacts[index]).append(formattedMobile);
+		$(idContacts[index]).append(formattedGithub);
+		$(idContacts[index]).append(formattedLocation);
+	}
+	
+	$("#header").prepend(formattedRole);
+	$("#header").prepend(formattedName);
+	$("#header").append(formattedPicture);
+	$("#header").append(formattedWelcomeMsg);
+	
+	$("#header").append(HTMLskillsStart);	
+	if (bio.skills.length>=0){
+		for (var index=0; index<bio.skills.length; index++){
+			var formattedSkills=HTMLskills.replace("%data%",bio.skills[index]);
+			
+			$("#skills").append(formattedSkills);
+		}
+	}
+}
+
+var education={
+	"schools":[{
+		"name":"Instituto Tecnológico de Costa Rica",
+		"location":"Cartago",
+		"degree":"Bachelor of Computer Engineering",
+		"majors":["BA"],
+		"dates":2012,
+		"url":"http://www.tec.ac.cr/"
+	}],
+	"onlineCourses":[
+	{
+		"title":"Intro to Computer Science",
+		"school":"Udacity",
+		"date":2016,
+		"url":""
+	},
+	{
+		"title":"Javascripts basics",
+		"school":"Udacity",
+		"date":2016,
+		"url":"https://classroom.udacity.com/courses/ud804/"
+	}
+	]
+};
+
+education.display = function(){
+	for (var index=0; index< education.schools.length; index++){
+		var formattedSchoolName=HTMLschoolName.replace("%data%",education.schools[index].name);
+		var formattedSchoolDegree=HTMLschoolDegree.replace("%data%",education.schools[index].degree);
+		var formattedSchoolDates=HTMLschoolDates.replace("%data%",education.schools[index].dates);
+		var formattedSchoolLocation=HTMLschoolLocation.replace("%data%",education.schools[index].location);
+		var formattedSchoolMajor=HTMLschoolMajor.replace("%data%",education.schools[index].majors);
+		var formattedSchoolURL=HTMLschoolURL.replace("%data%",education.schools[index].url);
+		
+		$("#education").append(HTMLschoolStart);
+		$(".education-entry:last").append(formattedSchoolName);
+		$(".education-entry:last").append(formattedSchoolDegree);
+		$(".education-entry:last").append(formattedSchoolDates);
+		$(".education-entry:last").append(formattedSchoolLocation);
+		$(".education-entry:last").append(formattedSchoolMajor);
+		$(".education-entry:last").append(formattedSchoolURL);
+	}
+	
+	$("#education").append(HTMLonlineClasses);
+	for (var index=0; index< education.onlineCourses.length; index++){
+		var formattedOnlineCourseName=HTMLonlineTitle.replace("%data%",education.onlineCourses[index].title);
+		var formattedOnlineCourseSchool=HTMLonlineSchool.replace("%data%",education.onlineCourses[index].school);
+		var formattedOnlineCourseDates=HTMLonlineDates.replace("%data%",education.onlineCourses[index].date);
+		var formattedOnlineCourseURL=HTMLonlineURL.replace("%data%",education.onlineCourses[index].url);
+		
+		$("#education").append(HTMLschoolStart);
+		$(".education-entry:last").append(formattedOnlineCourseName);
+		$(".education-entry:last").append(formattedOnlineCourseSchool);
+		$(".education-entry:last").append(formattedOnlineCourseDates);
+		$(".education-entry:last").append(formattedOnlineCourseURL);
+	}
+}
+
+var work ={
+	"jobs":[
+	{
+		"employer":"Ncubo",
+		"title":"Software Developer",
+		"location":"Cartago",
+		"dates":"2015-Current",
+		"description":"I work on the company's own Bambu system and other outsourcing systems, adding new features, integrating existing libraries and other systems like ERP (Exactus) using .NET web technology"
+	},
+	{
+		"employer":"Fairplay Labs",
+		"title":"Software Developer",
+		"location":"Zapote",
+		"dates":"2012-2014",
+		"description":"I worked on 5 game projects using Unity3d, Monodevelop and C# language. I implemented new features to the applications and fixing bugs."
+	},
+	]
+};
+
+work.display = function(){
+	work.jobs.forEach(function(job){
+		var formattedEmployer=HTMLworkEmployer.replace("%data%",job.employer);
+		var formattedTitle=HTMLworkTitle.replace("%data%",job.title);
+		var formattedWorkDates=HTMLworkDates.replace("%data%",job.dates);
+		var formattedWorkLocation=HTMLworkLocation.replace("%data%",job.location);
+		var formattedWorkDescription=HTMLworkDescription.replace("%data%",job.description);
+		
+		$("#workExperience").append(HTMLworkStart);
+		$(".work-entry:last").append(formattedEmployer+formattedTitle);
+		$(".work-entry:last").append(formattedWorkDates);
+		$(".work-entry:last").append(formattedWorkLocation);
+		$(".work-entry:last").append(formattedWorkDescription);
+	});
+}
+
+var projects={
+	"projects":[
+	{
+		"title":"A game",
+		"dates":2015,
+		"description":"It was about spaceships combat",
+		"images":[]
+	},
+	{
+		"title":"My resume",
+		"dates":2016,
+		"description":"This one",
+		"images":["images/project2.png"]
+	}
+]};
+
+projects.display = function(){
+	for (var index=0; index< projects.projects.length; index++){
+		var formattedProjectName=HTMLprojectTitle.replace("%data%",projects.projects[index].title);
+		var formattedProjectDates=HTMLprojectDates.replace("%data%",projects.projects[index].dates);
+		var formattedProjectDescription=HTMLprojectDescription.replace("%data%",projects.projects[index].description);
+		
+		$("#projects").append(HTMLprojectStart);
+		$(".project-entry:last").append(formattedProjectName);
+		$(".project-entry:last").append(formattedProjectDates);
+		$(".project-entry:last").append(formattedProjectDescription);
+		
+		for (var indexImage=0; indexImage<projects.projects[index].images.length; indexImage++){
+			var formattedImages=HTMLprojectImage.replace("%data%",projects.projects[index].images[indexImage]);
+			
+			$(".project-entry:last").append(formattedImages);
+		}
+	}
+}
+
+bio.display();
+education.display();
+work.display();
+projects.display();
